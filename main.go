78df9b341cac3bfb77cd8f2a1959098898c15553
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2019-09-24 09:39:02 +0700

    Connect API with mysql database

--- main.go
+++ main.go
@@ -9,10 +9,13 @@ We import 4 important libraries
 
 import (
 	"bytes"
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"html/template"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -33,6 +36,8 @@ type Customer struct {
 }
 
 var customers []Customer
+var db *sql.DB
+var err error
 
 /**
 	Controller to handle Welcome page
@@ -101,15 +106,23 @@ func balancesController(w http.ResponseWriter, r *http.Request) {
 
 // Go application entrypoint
 func main() {
+	//db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/service_support_db")
+	db ,err = sql.Open("mysql", "root:@/service_support_db")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer db.Close()
 	/**
 		With Gorilla Mux Router
 	 */
 	router := mux.NewRouter()
 
 	// Mock Data
-	customers = append(customers, Customer{ID: "1", Name: "Fadlika Dita Nurjanto", Phone: "0851672719", Email: "fadlikadn@gmail.com" })
+	/*customers = append(customers, Customer{ID: "1", Name: "Fadlika Dita Nurjanto", Phone: "0851672719", Email: "fadlikadn@gmail.com" })
 	customers = append(customers, Customer{ID: "2", Name: "Fauzan Ibnu Prihadiyono", Phone: "0851672789", Email: "fauzanibnup@gmail.com" })
-	customers = append(customers, Customer{ID: "3", Name: "Fauzi Triagung Wiguna", Phone: "0812671872", Email: "fauzitri@gmail.com" })
+	customers = append(customers, Customer{ID: "3", Name: "Fauzi Triagung Wiguna", Phone: "0812671872", Email: "fauzitri@gmail.com" })*/
 
 	router.HandleFunc("/", welcomeController).Methods("GET")
 	router.HandleFunc("/balances", balancesController).Methods("POST")
@@ -141,19 +154,36 @@ func main() {
 func deleteCustomer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for index, item := range customers {
+
+	// Using memory
+	/*for index, item := range customers {
 		if item.ID == params["id"] {
 			customers = append(customers[:index], customers[index+1:]...)
 			break
 		}
 	}
-	json.NewEncoder(w).Encode(customers)
+	json.NewEncoder(w).Encode(customers)*/
+
+	// Using database
+	stmt, err := db.Prepare("DELETE FROM customers WHERE id  = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	_, err = stmt.Exec(params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "Post with ID = %s was deleted", params["id"])
 }
 
 func updateCustomer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for index, item := range customers {
+
+	// Using memory
+	/*for index, item := range customers {
 		if item.ID == params["id"] {
 			customers = append(customers[:index], customers[index+1:]...)
 
@@ -166,32 +196,125 @@ func updateCustomer(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
-	json.NewEncoder(w).Encode(customers)
+	json.NewEncoder(w).Encode(customers)*/
+
+	// Using database
+	stmt, err := db.Prepare("UPDATE customers SET name = ?, phone = ?, email = ? WHERE id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	newName := keyVal["name"]
+	newPhone := keyVal["phone"]
+	newEmail := keyVal["email"]
+
+	_, err = stmt.Exec(newName, newPhone, newEmail, params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "Customer with ID = %s was updated", params["id"])
 }
 
 func getCustomer(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	for _, item := range customers {
+
+	// Using memory
+	/*for _, item := range customers {
 		if item.ID == params["id"] {
 			json.NewEncoder(w).Encode(item)
 			return
 		}
 	}
-	json.NewEncoder(w).Encode(&Customer{})
+	json.NewEncoder(w).Encode(&Customer{})*/
+
+	// Using database
+	result, err := db.Query("SELECT id, name, phone, email FROM customers where id = ?", params["id"])
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer result.Close()
+
+	var customer Customer
+
+	for result.Next() {
+		err := result.Scan(&customer.ID, &customer.Name, &customer.Phone, &customer.Email)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	json.NewEncoder(w).Encode(customer)
 }
 
 func createCustomers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var customer Customer
+
+	// Using Memory
+	/*var customer Customer
 	_ = json.NewDecoder(r.Body).Decode(&customer)
 	customer.ID = strconv.Itoa(rand.Intn(1000000))
 	customers = append(customers, customer)
-	json.NewEncoder(w).Encode(&customer)
+	json.NewEncoder(w).Encode(&customer)*/
+
+	// Using MySQL
+	stmt, err := db.Prepare("INSERT INTO customers(id, name, phone, email) VALUES(?, ?, ?, ?)")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	body, err:= ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal)
+	name := keyVal["name"]
+	phone := keyVal["phone"]
+	email := keyVal["email"]
+	id := strconv.Itoa(rand.Intn(1000000))
+
+	_, err = stmt.Exec(id, name, phone, email)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fmt.Fprintf(w, "New customer was created")
 }
 
 func getCustomers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+
+	// GET FROM MySQL
+	var customers []Customer
+
+	result, err := db.Query("SELECT id, name, phone, email from customers")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer result.Close()
+
+	for result.Next() {
+		var customer Customer
+		err := result.Scan(&customer.ID, &customer.Name, &customer.Phone, &customer.Email)
+		if err != nil {
+			//panic(err.Error())
+			panic(err.Error())
+		}
+		customers = append(customers, customer)
+	}
+
 	json.NewEncoder(w).Encode(customers)
 }
 
